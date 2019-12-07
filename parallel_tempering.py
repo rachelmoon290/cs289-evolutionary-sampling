811344f3ca10f031b14efe40965962481c4946f4
Rachel Moon <rachelmoon@Rachels-MacBook-Pro.local>
2019-12-06 19:48:27 -0500

    fixed error

--- parallel_tempering.py
+++ parallel_tempering.py
@@ -6,42 +6,73 @@ import sys
 
 np.random.seed(289)
 
-# set parameters for target/proposal distribution
-# mu0 = 1
-# mu1 = 10
 
-# target_sigma0 = 1
-# target_sigma1 = 2
+"""Initialize target distribution"""
 
-# initialization of functions
+# set parameters
+mu0 = 1
+mu1 = 10
+
+target_sigma0 = 1
+target_sigma1 = 2
 
 # target distribution: normal case
 # f = lambda x: norm(mu0, target_sigma0).pdf(x)
+# energy = lambda x: -np.log(f(x))
 
 def energy(x):
     if f(x) < 1e-20:
-        return -1e10
+        return -np.log(1e-10)
     else:
         return -np.log(f(x))
 
 # target distribution: bimodal case
-# f = lambda x: 0.5*norm(mu0, target_sigma0).pdf(x) + 0.5*norm(mu1, target_sigma1).pdf(x)
+f = lambda x: 0.5*norm(mu0, target_sigma0).pdf(x) + 0.5*norm(mu1, target_sigma1).pdf(x)
 
-# # target distribution: difficult normal case
-mu0, mu1, mu2, mu3 = -10,0,10,30
-sig0, sig1, sig2, sig3 = 1, 2, 2, 1
-f = lambda x: 0.25*norm(mu0, sig0).pdf(x) + 0.25*norm(mu1, sig1).pdf(x) + 0.25*norm(mu2, sig2).pdf(x) + 0.25*norm(mu3, sig3).pdf(x)
+# target distribution: difficult normal case
+# mu0, mu1, mu2, mu3 = -10,0,10,30
+# sig0, sig1, sig2, sig3 = 1, 2, 2, 1
+# f = lambda x: 0.25*norm(mu0, sig0).pdf(x) + 0.25*norm(mu1, sig1).pdf(x) + 0.25*norm(mu2, sig2).pdf(x) + 0.25*norm(mu3, sig3).pdf(x)
 
-# Proposal distribution: normal distribution
+
+
+"""Proposal distribution"""
 proposal_sigma = 1
 proposal = lambda x: np.random.normal(x, proposal_sigma)
+
+
+
 
 def metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp):
     """
     Metropolis-Hastings accept-reject framework
     """
+
     #compute a probability for accepting new solution
     alpha = min(1, np.exp((old_energy - new_energy) / temp))
+
+
+    #MH sampling
+    if np.random.uniform() < alpha:
+        #update everything if new solution accepted
+        accepted = 1
+        return accepted, new_solution, new_energy
+
+    else:
+        # Keep the old stuff if new solution not accepted
+        accepted = 0
+        return accepted, old_solution, old_energy
+
+
+
+def metropolis_hastings_exchange(old_solution, new_solution, old_energy, new_energy, temp):
+    """
+    Metropolis-Hastings accept-reject framework for exchange
+    """
+
+    #compute a probability for accepting new solution
+    alpha = min(1, np.exp((old_energy - new_energy) * (1/temp_list[rank] - 1/temp_list[rank+1]) ))
+
 
     #MH sampling
     if np.random.uniform() < alpha:
@@ -60,12 +91,12 @@ def exchange(old_solution, energy, old_energy, temp):
     """
     exchanged = 0
     if rank > 0:
-        comm.send(old_solution, dest=rank-1, tag=13)
-    if rank < num_chains -1:
-        new_solution = comm.recv(source=rank+1, tag=13)
+        comm.send(old_solution, dest=rank-1)
+    if rank < num_chains-1:
+        new_solution = comm.recv(source=rank+1)
         new_energy = energy(new_solution)
-        exchanged, old_solution, old_energy = metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp)
-    
+        # exchanged, old_solution, old_energy = metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp)
+        exchanged, old_solution, old_energy = metropolis_hastings_exchange(old_solution, new_solution, old_energy, new_energy, temp)
     return exchanged, old_solution, old_energy
 
 def parallel_tempering(energy, proposal, init_sol, epochs, temp):
@@ -83,7 +114,7 @@ def parallel_tempering(energy, proposal, init_sol, epochs, temp):
     # exchanged_interval = int(epochs/1000)
 
     for epoch in range(epochs):
-        if epoch % 50 == 0 and epoch > 0:
+        if epoch % 500 == 0 and epoch > 0:
             comm.barrier()
             exchanged, old_solution, old_energy = exchange(np.array(old_solution), energy, old_energy, temp)
             total_exchanged += exchanged
@@ -110,37 +141,49 @@ if __name__ == '__main__':
 
     num_chains = comm.Get_size()
 
-    # initial solution
+
+    # initial solutions (samples)    
     #init_sol = np.random.choice(20,num_chains)
-    init_sol = np.random.randint(-10,20,size=num_chains)
+    init_sol = np.random.randint(-20,20,size=num_chains)
 
+    # choose rank to report time elapsed
     chosen_rank = 0
-
     if rank == chosen_rank:
         starttime = time.time()
 
+
+    # get cmd argument inputs
     try:
         num_epochs = int(sys.argv[1])
         assert num_epochs > 0
 
     except:
-        print('err')
+        print('Invalid number of epochs, should be non-zero positive integer.')
         num_epochs = 10
 
     # intialize temperature
-    if rank==0: 
-        temp = 1
-    else:
-        temp = rank * 1.5
+    # temp_list = [1,1.2,1.4,1.6,1.8,2,2.2,]
+    # temp = temp_list[rank]
+
+
+    temp_list = [1, 1.5, 3, 4.5, 6, 7.5]
+    temp = temp_list[rank]
+    
+    
+    # if rank==0: 
+    #     temp = 1
+    # else:
+    #     temp = rank * 1.5
     
     accumulator, ratio_accept, ratio_exchange = parallel_tempering(energy, proposal, init_sol, epochs=num_epochs, temp=temp)
     print(f'acceptance for temp={temp}: {ratio_accept*100:.2f}%, exchange: {ratio_exchange*100:.2f}%')
 
+    # wait until everyone is finished
     comm.barrier()
 
     if rank == chosen_rank:
         endtime = time.time() - starttime
-        print(f'Total time: {endtime}')
+        print(f'Total time elapsed: {endtime} seconds')
 
-    # writing files
+    # write results
     np.save(f'results/process_{rank}.npy', accumulator)
--- parallel_tempering_respawn.py
+++ parallel_tempering_respawn.py
@@ -7,29 +7,30 @@ import sys
 np.random.seed(289)
 
 # set target distribution
-# mu0, mu1 = 1, 10
+mu0, mu1 = 1, 10
 
-# target_sigma0, target_sigma1 = 1,2
+target_sigma0, target_sigma1 = 1,2
 
 # target distribution: normal case
 # f = lambda x: norm(mu0, target_sigma0).pdf(x)
 
 # target distribution: bimodal case
-# f = lambda x: 0.5*norm(mu0, target_sigma0).pdf(x) + 0.5*norm(mu1, target_sigma1).pdf(x)
+f = lambda x: 0.5*norm(mu0, target_sigma0).pdf(x) + 0.5*norm(mu1, target_sigma1).pdf(x)
 
 # energy function for target distribution
 def energy(x):
+    # set boundary to prevent numerical overflow
     if f(x) < 1e-20:
-        return -1e10
+        return -np.log(1e-10)
     else:
         return -np.log(f(x))
 
 
-mu0, mu1, mu2, mu3 = -10,0,10,30
-sig0, sig1, sig2, sig3 = 1, 2, 2, 1
+# mu0, mu1, mu2, mu3 = -10,0,10,30
+# sig0, sig1, sig2, sig3 = 1, 2, 2, 1
 
-# target distribution: difficult normal case
-f = lambda x: 0.25*norm(mu0, sig0).pdf(x) + 0.25*norm(mu1, sig1).pdf(x) + 0.25*norm(mu2, sig2).pdf(x) + 0.25*norm(mu3, sig3).pdf(x)
+# # target distribution: difficult normal case
+# f = lambda x: 0.25*norm(mu0, sig0).pdf(x) + 0.25*norm(mu1, sig1).pdf(x) + 0.25*norm(mu2, sig2).pdf(x) + 0.25*norm(mu3, sig3).pdf(x)
 
 
 # Set proposal distribution: normal distribution
@@ -66,30 +67,52 @@ def exchange(old_solution, energy, old_energy, temp):
     if rank < num_chains - 1:
         new_solution = comm.recv(source=rank+1, tag=13)
         new_energy = energy(new_solution)
-        exchanged, old_solution, old_energy = metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp)
+        exchanged, old_solution, old_energy = metropolis_hastings_exchange(old_solution, new_solution, old_energy, new_energy, temp)
 
     return exchanged, old_solution, old_energy
+
+
+def metropolis_hastings_exchange(old_solution, new_solution, old_energy, new_energy, temp):
+    """
+    Metropolis-Hastings accept-reject framework for exchange
+    """
+
+    #compute a probability for accepting new solution
+    alpha = min(1, np.exp((old_energy - new_energy) * (1/temp_list[rank] - 1/temp_list[rank+1]) ))
+
+
+    #MH sampling
+    if np.random.uniform() < alpha:
+        #update everything if new solution accepted
+        accepted = 1
+        return accepted, new_solution, new_energy
+
+    else:
+        # Keep the old stuff if new solution not accepted
+        accepted = 0
+        return accepted, old_solution, old_energy
+
+
+
+def respawn(accumulator, old_solution, old_energy):
+    respawned = 0
+    recent_energy_vals = [element[1] for element in accumulator[-(respawn_rate):-1]]
+    if np.mean(recent_energy_vals) > 8 and rank !=0: # if samples are in general producing low energy for past few iterations
+        new_solution = np.random.randint(-20,20) # respawn at new initial point
+        new_energy = energy(new_solution)
+        respawned, old_solution, old_energy = metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp)
+
+    return old_solution, old_energy, respawned
 
 
 # def respawn(accumulator, old_solution, old_energy):
 #     respawned = 0
 #     recent_energy_vals = [element[1] for element in accumulator[-(respawn_rate):-1]]
 #     if np.mean(recent_energy_vals) < -10 and rank !=0: # if samples are in general producing low energy for past few iterations
-#         new_solution = np.random.randint(-20,20) # respawn at new initial point
-#         new_energy = energy(new_solution)
-#         respawned, old_solution, old_energy = metropolis_hastings(old_solution, new_solution, old_energy, new_energy, temp)
-
+#         old_solution = np.random.randint(-20,20) # respawn at new initial point
+#         old_energy = energy(old_solution)
+#         respawned = 1
 #     return old_solution, old_energy, respawned
-
-
-def respawn(accumulator, old_solution, old_energy):
-    respawned = 0
-    recent_energy_vals = [element[1] for element in accumulator[-(respawn_rate):-1]]
-    if np.mean(recent_energy_vals) < -10 and rank !=0: # if samples are in general producing low energy for past few iterations
-        old_solution = np.random.randint(-20,20) # respawn at new initial point
-        old_energy = energy(old_solution)
-        respawned = 1
-    return old_solution, old_energy, respawned
 
 def parallel_tempering(energy, proposal, init_sol, epochs, temp, exchange_rate, respawn_rate):
     """
@@ -164,13 +187,16 @@ if __name__ == '__main__':
         num_epochs = 10
 
     # intialize temperature
-    if rank==0: 
-        temp = 1
-    else:
-        temp = rank * 1.5
+
+    temp_list = [1, 1.5, 3, 4.5, 6, 7.5]
+    temp = temp_list[rank]
+    # if rank==0: 
+    #     temp = 1
+    # else:
+    #     temp = rank * 1.5
 
 
-    exchange_rate = 50
+    exchange_rate = 500
     respawn_rate = 100
 
     accumulator, ratio_accept, ratio_exchange, ratio_respawn = parallel_tempering(energy, proposal, init_sol, epochs=num_epochs, temp=temp, exchange_rate=exchange_rate, respawn_rate=respawn_rate)
